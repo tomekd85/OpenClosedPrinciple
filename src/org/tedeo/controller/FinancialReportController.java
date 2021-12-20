9465ee31c7abe2dbc3859d0aa86e16fc8fd6a14c
tomaszd <tdomachowski@adva.com>
2021-12-20 16:30:05 +0100

    create 1st uml with idea

--- src/org/tedeo/controller/FinancialReportController.java
+++ src/org/tedeo/controller/FinancialReportController.java
@@ -1,18 +1,29 @@
 package org.tedeo.controller;
 
 import org.tedeo.interactor.FinancialReportGenerator;
+import org.tedeo.pdfview.PdfView;
 import org.tedeo.printerpresenter.PrinterPresenter;
 import org.tedeo.screenpresenter.ScreenPresenter;
+import org.tedeo.webview.WebView;
 
 public class FinancialReportController {
 
+    public static void main(String[] args) {
+        final FinancialReportController financialReportController = new FinancialReportController();
+        financialReportController.generatePdfView();
+        financialReportController.generateWebView();
+    }
+
+
     public void generatePdfView(){
         FinancialEntities financialEntities = new FinancialReportGenerator().generateReportData();
-        new PrinterPresenter().show(financialEntities);
+        final PdfView pdfView = new PrinterPresenter().show(financialEntities);
+        pdfView.load();
     }
 
     public void generateWebView(){
         FinancialEntities financialEntities = new FinancialReportGenerator().generateReportData();
-        new ScreenPresenter().show(financialEntities);
+        final WebView webView = new ScreenPresenter().show(financialEntities);
+        webView.load();
     }
 }
